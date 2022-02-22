a799a3fe432b6ada5949d4ab14fb6f9b04ef2b94
Valerie Russell <vruss14@gmail.com>
2022-02-22 16:39:17 -0500

    create & format basic HTML form

--- pages/index.js
+++ pages/index.js
@@ -12,43 +12,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Roman Numeral Converter</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h2 className={styles.description}>
+          Input a Roman Numeral from 1-1000, and this app will convert it to an integer.
+          Enter any integer from 1-1000, and this app will convert it to a Roman Numeral.
+        </h2>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className={styles.wrapper}>
+          <form className={styles.form}>
+            <label htmlFor="user-input-number" className={styles.label}>Enter a number:</label>
+            <input type="text" id="user-input-number" name="user-input-number" />
+          </form> 
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <button className={styles.submit} type="submit">Submit</button>
         </div>
       </main>
 
